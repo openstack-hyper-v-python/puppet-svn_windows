2ce9429e6d4cc60ecbb2e4c0146ff5b0777a5970
gabloe <t-gabloe@microsoft.com>
2014-06-06 13:33:05 -0700

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+class svn_windows (
+    $version     = $svn_windows::params::version,
+    $url         = $svn_windows::params::url,
+    $package     = $svn_windows::params:package,
+    $file_path   = false,
+) inherits svn_windows::params {
+    if $file_path {
+        $svn_installer_path = $file_path
+    } else {
+        $svn_installer_path = "${::temp}\\${package}-${version}.msi"
+    }
+    windows_common::remote_file{'svn':
+        source      => $url,
+        destination => $svn_installer_path,
+        before      => Package[$package],
+    }
+        
+    package { $package:
+        ensure          => installed,
+        source          => $svn_installer_path,
+        install_options => ['/VERYSILENT','/SUPPRESSMSGBOXES','/LOG'],
+    }
+
+    $svn_path = 'C:\\Program Files (x86)\\SlikSvn\\bin'
+ 
+    windows_path { $svn_path:
+        ensure  => present,
+        require => Package[$package],
+    }
+}
---
+++ manifests/params.pp
@@ -0,0 +1,5 @@
+class svn_windows::params {
+    $version    = '1.8.9'
+    $url        = 'http://www.sliksvn.com/pub/Slik-Subversion-1.8.9-win32.msi'
+    $package    = 'sliksvn'
+}
